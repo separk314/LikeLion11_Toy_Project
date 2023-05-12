cd4e96f81145e9bda1a17156c5976fb38e35b402
soeun <separk314@naver.com>
2023-05-13 00:53:25 +0900

    Fix: CORS_ALLOWED_ORIGINS 수정

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,8 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [ 
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = "config.urls"
